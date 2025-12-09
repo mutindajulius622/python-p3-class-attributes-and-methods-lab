425d59315ddd3889c23ccde6bc2fee5e9a3c8dad
julius mutinda <juliusmwendwa082@gmail.com>
2025-12-09 15:09:15 +0300

    Update files

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,22 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 0
+        Song.genre_count[genre] += 1
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 0
+        Song.artist_count[artist] += 1
